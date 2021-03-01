cf7757955d334233e13b0c4ec8236535bf878608
LiaoLong <coderliaolong@outlook.com>
2021-03-02 01:12:00 +0800

    20210302

---
+++ src/Test.java
@@ -0,0 +1,9 @@
+/**
+ * @author coderliaolong@outlook.com
+ * @date 2021/3/2 1:10
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
